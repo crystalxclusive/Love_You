c647ed7c4fc69ad03f4df28cbf1f6271bfc22271
CrystalXclusive <78354782+crystalxclusive@users.noreply.github.com>
2022-12-13 14:11:39 +0100

    Add files via upload

---
+++ love_messages.py
@@ -0,0 +1,21 @@
+import random
+
+# Define a list of love messages
+love_messages = [
+    "I love you more than words can say",
+    "You are the most amazing person I have ever met",
+    "I am so lucky to have you by my side",
+    "I can't wait to meet our little one",
+    "You are the best thing that has ever happened to me",
+    "I love you more and more each day",
+    "I am so grateful for you and our growing family",
+    "You are beautiful inside and out",
+    "I am so excited to start this new chapter of our lives together",
+    "I am so proud of you and all that you do"
+]
+
+# Select a random message from the list
+message = random.choice(love_messages)
+
+# Print the message
+print(message)
